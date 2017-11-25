2fbbdcf8cc1e409d2222da19e3f8457928c49966
p-siriphanthong <p.siriphanthong@gmail.com>
2017-11-26 05:00:16 +0700

    Heroku deploy

--- m/insert_comment.php
+++ m/insert_comment.php
@@ -1,12 +1,11 @@
 <?php
 	// $conn = new mysqli('localhost', 'root', '', 'ywc');
-	// $url = parse_url(getenv('CLEARDB_DATABASE_URL'));
-	// $server = $url['host'];
-	// $username = $url['user'];
-	// $password = $url['pass'];
-	// $db = substr($url['path'], 1);
-	// $conn = new mysqli($server, $username, $password, $db);
-	$conn = new mysqli('us-cdbr-iron-east-05.cleardb.net', 'bcd77d33767975', 'be0ca615', 'heroku_e1704d2f0a23fb7');
+	$url = parse_url(getenv('CLEARDB_DATABASE_URL'));
+	$server = $url['host'];
+	$username = $url['user'];
+	$password = $url['pass'];
+	$db = substr($url['path'], 1);
+	$conn = new mysql($server, $username, $password, $db);
 
 	// Check connection
 	if(mysqli_connect_errno()) {
